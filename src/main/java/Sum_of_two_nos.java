4e1e45838531a804bce8e69ab05705dcc5665856
hp <prasantbhagavatula92@gmail.com>
2023-08-29 19:52:28 +0530

    Committing the java code

---
+++ src/main/java/Sum_of_two_nos.java
@@ -0,0 +1,10 @@
+public class Sum_of_two_nos {
+
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The sum is " +sum(10,100));
+    }
+}
